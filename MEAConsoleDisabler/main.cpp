63e1dc5735985e9e0fd0b3dafb6c65a402bf9743
Erik JS <erikjs_br@hotmail.com>
2017-04-21 20:47:13 -0300

    First commit

---
+++ MEAConsoleDisabler/main.cpp
@@ -0,0 +1,79 @@
+#include "main.h"
+
+BYTE pattern[] = {0xC7, 0x43, 0x08, 0x02, 0x00, 0x00, 0x00, 0x48, 0x83, 0xC4, 0x20, 0x5B, 0xC3};
+
+bool DataCompare(const BYTE* OpCodes, const BYTE* Mask, const char* StrMask)
+{
+	while (*StrMask)
+	{
+		if(*StrMask == 'x' && *OpCodes != *Mask )
+			return false;
+		++StrMask;
+		++OpCodes;
+		++Mask;
+	}
+	return true;
+}
+
+DWORD64 FindPattern(DWORD64 StartAddress, DWORD CodeLen, BYTE* Mask, const char* StrMask, unsigned short ignore)
+{
+	unsigned short Ign = 0;
+	DWORD i = 0;
+	while (Ign <= ignore)
+	{
+		if(DataCompare((BYTE*)(StartAddress + i++), Mask,StrMask))
+			++Ign;
+		else if (i>=CodeLen)
+			return 0;
+	}
+	return StartAddress + i - 1;
+}
+
+DWORD WINAPI Start(LPVOID lpParam)
+{
+    //Sleep(5000);
+    HMODULE game = GetModuleHandle(NULL);
+    DWORD64 startadd = (DWORD64)GetProcAddress(game, "JointPosMotionDataDistance");
+    if (!startadd)
+    {
+        //printf("MEAConsoleDisabler: startadd failed!\n");
+        return 0;
+    }
+    DWORD64 target = FindPattern(startadd, 0x500000, pattern, "xxxxxxxxxxxxx", 0);
+    if (!target)
+    {
+        //printf("MEAConsoleDisabler: target failed!\n");
+        return 0;
+    }
+    DWORD dwProtect;
+	VirtualProtect((void*)(target + 3), 0x1, PAGE_READWRITE, &dwProtect );
+	*(BYTE*)(target + 3) = 0;
+	VirtualProtect((void*)(target + 3), 0x1, dwProtect, &dwProtect );
+    //printf("MEAConsoleDisabler: done.\n");
+    return 0;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+    switch (fdwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+            // attach to process
+            // return FALSE to fail DLL load
+            CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Start, 0, 0, 0);
+            break;
+
+        case DLL_PROCESS_DETACH:
+            // detach from process
+            break;
+
+        case DLL_THREAD_ATTACH:
+            // attach to thread
+            break;
+
+        case DLL_THREAD_DETACH:
+            // detach from thread
+            break;
+    }
+    return TRUE; // successful
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# ASI Plugins for Mass Effect: Andromeda
+
+**Last game version tested:** build 727902 (Apr 06 2017)
+
+This repository is *supposed to be* a collection of various plugins (DLL files renamed to *.asi) which provide new features and functionalities to the game. The biggest advantage of ASI plugins is that it's easy for users to choose which features they want, and enable/disable them at any time. Anyone can make and distribute their own plugin.
+
+*Currently, only one plugin is here...*
+
+**An ASI Plugin requires the use of a separate ASI Loader in order to be used / Intended for use with proxy DLL available at https://github.com/Erik-JS/meandromeda-dll**
+
+**How to use a plugin:** simply drop MEAxxx.asi in the game's root folder, or an 'ASI' folder in the game's root folder. You're not limited in how many plugins you can use at a time.
+
+**Disabling a plugin:** delete the respective .asi file. Alternatively, you can instead move it to another folder, or rename it so it doesn't end with ".asi" (or, if you're using 'ASI' folder, rename it to disable all plugins).
+
+-----------------------------------------------------------
+
+**Binaries only:** [Dropbox](https://www.dropbox.com/sh/fdwv47v0jbsvpgl/AABiPMo3gwIUHuvuo7YjWKBCa?dl=0)
+
+This repository is not maintained or supported by EA or BioWare.<br />
+Mass Effect is a trademark of EA International (Studio and Publishing) Ltd.
